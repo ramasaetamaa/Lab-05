3a3808926e023b06422280df5c41ab9a2d07ea2f
Asus ROG Strix G15 <Asus ROG Strix G15@LAPTOP-HA1LS1TS>
2023-10-08 14:46:32 +0700

    Add project files.

---
+++ Faktorial/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int angka, hasil, i;
+
+	cout << "Input Angka: ";
+	cin >> angka;
+
+	hasil = 1;
+	for (i = 1; i <= angka; i++)
+	{
+		hasil = hasil * i;
+	}
+	cout << angka << "! = " << hasil;
+	cout << endl;
+
+	system("pause");
+
+	return 0;
+}
